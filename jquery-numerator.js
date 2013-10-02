dd354aced7acffbfd2c83528d40b99a4cd8db9df
garethdn <garethdn@gmail.com>
2013-10-03 00:33:15 +0100

    first commit. plugin only working the first time

---
+++ jquery-numerator.js
@@ -0,0 +1,105 @@
+;(function ( $, window, document, undefined ) {
+
+    var pluginName = "numerator",
+    defaults = {
+        easing: 'swing',
+        duration: 500,
+        delimiter: undefined,
+        rounding: 0,
+        toValue: undefined
+    };
+
+    function Plugin ( element, options ) {
+        this.element = element;
+        this.settings = $.extend( {}, defaults, options );
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    Plugin.prototype = {
+        init: function () {
+            this.parseElement();
+            this.setValue();
+        },
+
+        setValue: function() {
+            var self = this;
+
+            $({value: self.fromValue}).animate({value: self.settings.toValue}, {
+                duration: self.settings.duration,
+                easing: self.settings.easing,
+                step: function(now, fx) {
+                    $(self.element).text(Math.floor(now));
+                }
+            });
+        },
+
+        parseElement: function () {
+            var elText = $(this.element).text().trim();
+
+            if (this.settings.rounding < 1) {
+                this.fromValue = parseInt(elText);
+            } else {
+                this.fromValue = parseFloat(elText).toFixed(this.settings.rounding);
+            }
+
+        }
+    };
+
+    $.fn[pluginName] = function ( options ) {
+        var args = arguments;
+
+        // Is the first parameter an object (options), or was omitted,
+        // instantiate a new instance of the plugin.
+        if (options === undefined || typeof options === 'object') {
+            return this.each(function () {
+
+                // Only allow the plugin to be instantiated once,
+                // so we check that the element has no plugin instantiation yet
+                if (!$.data(this, 'plugin_' + pluginName)) {
+
+                    // if it has no instance, create a new one,
+                    // pass options to our plugin constructor,
+                    // and store the plugin instance
+                    // in the elements jQuery data object.
+                    $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+                }
+            });
+
+        // If the first parameter is a string and it doesn't start
+        // with an underscore or "contains" the `init`-function,
+        // treat this as a call to a public method.
+        } else if (typeof options === 'string' && options[0] !== '_' && options !== 'init') {
+
+            // Cache the method call
+            // to make it possible
+            // to return a value
+            var returns;
+
+            this.each(function () {
+                var instance = $.data(this, 'plugin_' + pluginName);
+
+                // Tests that there's already a plugin-instance
+                // and checks that the requested public method exists
+                if (instance instanceof Plugin && typeof instance[options] === 'function') {
+
+                    // Call the method of our plugin instance,
+                    // and pass it the supplied arguments.
+                    returns = instance[options].apply( instance, Array.prototype.slice.call( args, 1 ) );
+                }
+
+                // Allow instances to be destroyed via the 'destroy' method
+                if (options === 'destroy') {
+                  $.data(this, 'plugin_' + pluginName, null);
+                }
+            });
+
+            // If the earlier cached method
+            // gives a value back return the value,
+            // otherwise return this to preserve chainability.
+            return returns !== undefined ? returns : this;
+        }
+    };
+
+})( jQuery, window, document );
