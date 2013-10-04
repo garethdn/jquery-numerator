b8c247df3753f53706d86077bf859335ec201b78
garethdn <garethdn@gmail.com>
2013-10-04 16:16:00 +0100

    new demo page skeleton

---
+++ javascripts/jquery-numerator.js
@@ -0,0 +1,80 @@
+;(function ( $, window, document, undefined ) {
+
+    var pluginName = "numerator",
+    defaults = {
+        easing: 'swing',
+        duration: 500,
+        delimiter: undefined,
+        rounding: 0,
+        toValue: undefined,
+        fromValue: undefined,
+        onStart: function(){},
+        onStep: function(){},
+        onProgress: function(){},
+        onComplete: function(){}
+    };
+
+    function Plugin ( element, options ) {
+        this.element = element;
+        this.settings = $.extend( {}, defaults, options );
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    Plugin.prototype = {
+        init: function () {
+            this.parseElement();
+            this.setValue();
+        },
+
+        parseElement: function () {
+            var elText = $(this.element).text().trim();
+
+            this.settings.fromValue = this.format(elText);
+        },
+
+        setValue: function() {
+            var self = this;
+
+            $({value: self.settings.fromValue}).animate({value: self.settings.toValue}, {
+
+                duration: parseInt(self.settings.duration),
+
+                easing: self.settings.easing,
+
+                start: self.settings.onStart,
+
+                step: function(now, fx) {
+                    $(self.element).text(self.format(now));
+                    // accepts two params - (now, fx)
+                    self.settings.onStep(now, fx);
+                },
+
+                // accepts three params - (animation object, progress ratio, time remaining(ms))
+                progress: self.settings.onProgress,
+
+                complete: self.settings.onComplete
+            });
+        },
+
+        format: function(value){
+            if (this.settings.rounding < 1) {
+                return parseInt(value);
+            } else {
+                return parseFloat(value).toFixed(this.settings.rounding);
+            }
+        }
+    };
+
+    $.fn[ pluginName ] = function ( options ) {
+        return this.each(function() {
+            if ( $.data( this, "plugin_" + pluginName ) ) {
+                $.data(this, 'plugin_' + pluginName, null);
+            }
+            $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+
+        });
+    };
+
+})( jQuery, window, document );
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
-console.log('This would be the main JS file.');
+(function(){
+
+	$(document).on('click', '#numerate', parseForm);
+
+	function parseForm(e) {
+		e.preventDefault();
+		var options = {};
+
+		$.each($('#numerator-settings input'), function(i, val){
+			options[$(val).attr('name')] = $(val).val();
+		});
+
+		$.each($('#numerator-settings select'), function(i, val){
+			options[$(val).attr('name')] = $(val).val();
+		});
+
+		console.log(options);
+		$('#demo-display p').numerator(options);
+	}
+
+})();
