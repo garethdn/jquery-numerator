fd61e25d4350746caee713d8a73959e6741493e5
garethdn <garethdn@gmail.com>
2013-10-04 16:37:12 +0100

    Added version and author details to plugin script

--- jquery-numerator.js
+++ jquery-numerator.js
@@ -1,3 +1,17 @@
+/* 
+ *   jQuery Numerator Plugin 0.1.0
+ *   https://github.com/garethdn/jquery-numerator
+ *
+ *   Copyright 2013, Gareth Nolan
+ *   http://ie.linkedin.com/in/garethnolan/
+
+ *   Based on jQuery Boilerplate by Zeno Rocha with the help of Addy Osmani
+ *   http://jqueryboilerplate.com
+ *
+ *   Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/MIT
+ */
+
 ;(function ( $, window, document, undefined ) {
 
     var pluginName = "numerator",
